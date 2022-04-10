08f232605858c1e331e101dfc8ab7b310777339e
Sandro Darnuzer <sandro.darnuzer@gmail.com>
2022-04-10 21:55:35 +0200

    add api & make nice cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import FlashcardList from "./FlashcardList";
 import './App.css';
+import axios from "axios";
 
 export default function App() {
 
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
 
+
+  useEffect(() => {
+    axios
+      .get('https://opentdb.com/api.php?amount=10')
+      .then(res => {
+        setFlashcards(res.data.results.map((questionItem, index) => {
+          const answer = decodeString(questionItem.correct_answer)
+          const options = [
+            ...questionItem.incorrect_answers.map(a => decodeString(a)),
+            answer
+          ]
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(questionItem.question),
+            answer,
+            options: options.sort(() => Math.random() - 0.5)
+          }
+        }))
+      })
+  }, [])
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
   return (
-    <FlashcardList flashcards={flashcards}/>
+    <div className="container">
+      <FlashcardList flashcards={flashcards}/>
+    </div>
   )
 }
 
--- src/Flashcard.jsx
+++ src/Flashcard.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useRef } from 'react'
 import { useState } from 'react'
 
 export default function Flashcard({ flashcard }) {
   const [flip, setFlip] = useState(false)
+  const [height, setHeight] = useState('initial')
   
+  const frontEl = useRef()
+  const backEl = useRef()
+
+  function setMaxHeight() {
+    const frontHeight = frontEl.current.getBoundingClientRect().height
+    const backHeight = backEl.current.getBoundingClientRect().height
+    setHeight(Math.max(frontHeight, backHeight, 100))
+  }
+
+  useEffect(setMaxHeight, [flashcard.question, flashcard.answer, flashcard.options])
+  useEffect(() => {
+    window.addEventListener('resize', setMaxHeight)
+    return () => window.removeEventListener('resize', setMaxHeight)
+  }, [])
   return (
     <div
       className={`card ${flip ? 'flip' : ''}`}
+      style={{ height }}
       onClick={() => setFlip(!flip)}
     >
-      <div className="front">
+      <div className="front" ref={frontEl}>
         {flashcard.question}
         <div className="flashcard-options">
           {flashcard.options.map(option => {
@@ -17,7 +35,7 @@ export default function Flashcard({ flashcard }) {
           })}
         </div>
       </div>
-      <div className="back">
+      <div className="back" ref={backEl}>
         {flashcard.answer}
       </div>
     </div>
