2d70a6ad8049f9d80da62b0e6c2878b64a1648ca
Sandro Darnuzer <sandro.darnuzer@gmail.com>
2022-04-10 21:14:35 +0200

    pass data

--- src/Flashcard.jsx
+++ src/Flashcard.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default function Flashcard() {
+export default function Flashcard({ flashcard }) {
   return (
-    <div>Flashcard</div>
+    <div>
+      {flashcard.question}
+    </div>
   )
 }
